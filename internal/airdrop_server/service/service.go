839afda79558e0c6fc60918bcb68cac258bd5f52
rzry <1319339638@qq.com>
2021-09-18 11:17:19 +0800

    fix:fix
    Signed-off-by: rzry <1319339638@qq.com>

--- internal/airdrop_server/service/service.go
+++ internal/airdrop_server/service/service.go
@@ -31,11 +31,12 @@ func Airdrop(opts *options.Options) {
 		log.Error("链接token合约出错", zap.Error(err))
 		return
 	}
-	if balance, err := platfromInstance.BalanceOf(nil, opts.MainAddress); decimal.Zero.GreaterThan(decimal.NewFromBigInt(balance, 0)) ||
-		err != nil {
+	 balance, err := platfromInstance.BalanceOf(nil, opts.MainAddress)
+	 if decimal.Zero.GreaterThan(decimal.NewFromBigInt(balance, 0)) || err != nil {
 		log.Error("输入的主地址私钥余额为空,或有错误", zap.Error(err))
 		return
 	}
+	log.Info("当前地址余额为",zap.Any("balance",pkg.FromEthers(balance.String(),18)))
 	auth, err := pkg.Auth(*fflag.Private, fflag.ChainId, opts.Client)
 	if err != nil {
 		log.Error("获取私钥签名出错,请检查私钥", zap.Error(err))
@@ -47,28 +48,26 @@ func Airdrop(opts *options.Options) {
 	syncCalculateSum := func() {
 		defer wg.Done()
 		indexes := atomic.AddInt64(&index,1)
-		auth, err := pkg.Auth(*fflag.Private, fflag.ChainId, opts.Client)
+		auths, err := pkg.Auth(*fflag.Private, fflag.ChainId, opts.Client)
 		if err != nil {
 			log.Error("转账失败", zap.Error(err),zap.Any("user",waitList[indexes-1]))
 			log.Error("获取私钥签名出错,请检查私钥", zap.Error(err))
 			return
 		}
-		nonce := atomic.AddInt64(&sum, 1)
-		auth.Nonce = big.NewInt(nonce)
-		tx, err := platfromInstance.Transfer(auth, common.HexToAddress(strings.TrimSpace(waitList[indexes-1])),
+		auths.Nonce = big.NewInt(atomic.AddInt64(&sum, 1))
+		tx, err := platfromInstance.Transfer(auths, common.HexToAddress(strings.TrimSpace(waitList[indexes-1])),
 			amount.BigInt())
 		if err != nil {
 			log.Error("转账失败", zap.Error(err),zap.Any("user",waitList[indexes-1]))
 			return
 		}
-		log.Infof("转账给%s,交易hash为%s", strings.TrimSpace(waitList[indexes-1]), tx.Hash())
+		log.Infof("转账给%s,交易hash为%s,当前nonce为%d", strings.TrimSpace(waitList[indexes-1]), tx.Hash(),auths.Nonce.Int64())
 	}
 	waitDrops := ReadFile(*fflag.Path)
 
 	p, _ := ants.NewPoolWithFunc(int(*fflag.Goroutine), func(i interface{}) {
 		syncCalculateSum()
 	})
-
 	for k, waitDrop := range waitDrops {
 		if !strings.HasPrefix(strings.TrimSpace(waitDrop), "0x"){
 			log.Info("并非为地址,跳过此行",zap.Any("数值",strings.TrimSpace(waitDrop)))
@@ -82,6 +81,7 @@ func Airdrop(opts *options.Options) {
 
 	//转账结束后,获取错误行补发.
 	addtransfer := ReadAddFile("./airdrop.log")
+	time.Sleep(10*time.Second)
 	log.Info("转账结束后,获取错误行补发",zap.Any("错误数",len(addtransfer)))
 	authd, err := pkg.Auth(*fflag.Private, fflag.ChainId, opts.Client)
 	if err != nil {
@@ -89,15 +89,22 @@ func Airdrop(opts *options.Options) {
 		return
 	}
 	for _,v := range addtransfer{
-		nonce := atomic.AddInt64(&sum, 1)
-		authd.Nonce = big.NewInt(nonce)
+		authd.Nonce = authd.Nonce.Add(authd.Nonce,big.NewInt(1))
 		tx, err := platfromInstance.Transfer(authd, common.HexToAddress(strings.TrimSpace(v)),
 			amount.BigInt())
 		if err != nil {
 			log.Error("补发失败,请手动补发", zap.Error(err),zap.Any("user",v))
-			return
+			continue
 		}
-		log.Infof("补发给%s,交易hash为%s", strings.TrimSpace(v), tx.Hash())
+		log.Infof("补发给%s,交易hash为%s,当前nonce为%d", strings.TrimSpace(v), tx.Hash(),authd.Nonce.Int64())
 	}
 	log.Info("补发结束")
+	balance, err = platfromInstance.BalanceOf(nil, opts.MainAddress)
+	if  err != nil {
+		log.Error("输入的主地址私钥余额为空,或有错误", zap.Error(err))
+		return
+	}
+	time.Sleep(10*time.Second)
+	log.Info("当前地址余额为",zap.Any("balance",pkg.FromEthers(balance.String(),18)))
+
 }
